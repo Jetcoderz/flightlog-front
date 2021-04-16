1d618efca0d6e34e9bea0b4b9cb1c1ffa64b43e6
flying-coder <mayashinagawa@gmail.com>
2021-04-16 14:18:26 +0900

    Flight Component done

--- components/Flight.js
+++ components/Flight.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 import Picture from "./Picture";
+import FlightInfo from "./FlightInfo"
+import UserInfo from "./UserInfo";
 
 export default function Flight() {
   return (
     <View style={{ flex: 1 }}>
+      <FlightInfo />
+      <UserInfo />
       <Picture />
-      <Text>Test</Text>
     </View>
   );
 }
