3c0a392c7e3a4524b5f0ae9719800c8303b3e8c8
Yu Chu <cyjeff@gmail.com>
2021-04-16 12:28:51 +0900

    added page navigation to flightList

--- components/FlightList.js
+++ components/FlightList.js
@@ -1,25 +1,52 @@
 import React, { Component } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, Button } from "react-native";
 import { ListItem } from "react-native-elements";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Flight from "./Flight";
 
 export default function FlightList() {
   const state = useSelector((state) => state);
 
+  function CreateList({ navigation }) {
+    const list = state.flightList.map((l, i) => (
+      <ListItem key={i} bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title>{l.flightNo}</ListItem.Title>
+          <ListItem.Subtitle>
+            {l.dep}-{l.arr}
+          </ListItem.Subtitle>
+          <Button
+            title="Go to Details"
+            onPress={() => navigation.navigate("Details")}
+          />
+        </ListItem.Content>
+      </ListItem>
+    ));
+    return list;
+  }
+
+  function List({ navigation }) {
+    return (
+      <View style={{ flex: 1, backgroundColor: "#fff" }}>
+        <ScrollView>
+          <CreateList navigation={navigation}></CreateList>
+        </ScrollView>
+      </View>
+    );
+  }
+
+  function Details() {
+    return <Flight></Flight>;
+  }
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={{ flex: 1, backgroundColor: "#fff" }}>
-      <ScrollView>
-        {state.flightList.map((l, i) => (
-          <ListItem key={i} bottomDivider>
-            <ListItem.Content>
-              <ListItem.Title>{l.flightNo}</ListItem.Title>
-              <ListItem.Subtitle>
-                {l.dep}-{l.arr}
-              </ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-      </ScrollView>
-    </View>
+    <Stack.Navigator initialRouteName="List">
+      <Stack.Screen name="List" component={List} />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
   );
 }
