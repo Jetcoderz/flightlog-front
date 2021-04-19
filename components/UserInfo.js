1f7049023d74e08c5e56f8de4b84a3ddf952376b
flying-coder <mayashinagawa@gmail.com>
2021-04-19 14:09:02 +0900

    package added

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function UserInfo () {
+    // const [purpose, SetPurpose] = useState("")
+
+    // useEffect(() => {
+
+    // },[purpose]);
+
     return (
         <View>
-            <Text>UserInfo</Text>
+            <Text>Purpose:</Text>
+            <DropDownPicker 
+                items = {
+                    {label: 'Business'},
+                    {label: 'Travel'}
+                }
+            />
         </View>
     )
 }
