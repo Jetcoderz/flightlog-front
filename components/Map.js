e28b8123eac0b38799bff0346624c73f30ec9c42
Francesca Marino <fjmarino90@gmail.com>
2021-04-19 15:02:36 +0900

    got map showing lines for each flight, but need to fix rerendering still

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,50 @@
-import * as React from "react";
-import MapView from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import MapView, { Marker, Polyline } from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+const { airportList } = require("./airportList");
 
 export default function Map() {
+  const [pathCoords, setPathCoords] = useState([]);
+  const [airports, setAirports] = useState([]);
+  const state = useSelector((state) => state);
+
+  useEffect(() => {
+    for (const flight of state.flightList) {
+      if (!airports.includes(flight.depAirport)) {
+        airports.push(flight.depAirport);
+      }
+      if (!airports.includes(flight.arrAirport)) {
+        airports.push(flight.arrAirport);
+      }
+      pathCoords.push([
+        {
+          latitude: airportList[flight.depAirport].Latitude,
+          longitude: airportList[flight.depAirport].Longitude,
+        },
+        {
+          latitude: airportList[flight.arrAirport].Latitude,
+          longitude: airportList[flight.arrAirport].Longitude,
+        },
+      ]);
+    }
+  }, []);
+
+  const markers = airports.map((airport) => {
+    return (
+      <Marker
+        coordinate={{
+          latitude: airportList[airport].Latitude,
+          longitude: airportList[airport].Longitude,
+        }}
+      />
+    );
+  });
+
+  const paths = pathCoords.map((path) => {
+    return <Polyline coordinates={path} />;
+  });
+
   return (
     <View style={styles.container}>
       <MapView
@@ -13,7 +55,10 @@ export default function Map() {
           latitudeDelta: 10,
           longitudeDelta: 10,
         }}
-      />
+      >
+        {markers}
+        {paths}
+      </MapView>
     </View>
   );
 }
