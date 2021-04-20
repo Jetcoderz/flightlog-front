1a5956b88413c84026c17eae5a04ace73fde5139
flying-coder <mayashinagawa@gmail.com>
2021-04-20 10:32:46 +0900

    made card for flight info

--- components/Flight.js
+++ components/Flight.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import Picture from "./Picture";
 import { useSelector, useDispatch } from "react-redux";
 import FlightInfo from "./FlightInfo";
 import UserInfo from "./UserInfo";
+import { color } from "react-native-reanimated";
 
 export default function Flight() {
   const state = useSelector((state) => state);
@@ -13,16 +14,74 @@ export default function Flight() {
   )[0];
 
   return (
-    <View style={{ flex: 1 }}>
-      <Image source={state.logo[thisFlight.airlineICAO]}></Image>
-      <Text>{thisFlight.airlineICAO}</Text>
-      <Text>Flight Details</Text>
-      <Text>{state.selectedFlight}</Text>
-      <Text>Departure: {thisFlight.depAirport}</Text>
-      <Text>Arrival: {thisFlight.arrAirport}</Text>
-      {/* <FlightInfo /> */}
-      <UserInfo thisFlight = {thisFlight}/>
+    <View style={styles.flight}>
+      <View style={styles.flightInfo}>
+        <View style={styles.logoAirline}>
+          <Image source={state.logo[thisFlight.airlineICAO]} style={styles.logo}></Image>
+          <Text style={styles.airline}>{thisFlight.airlineICAO}</Text>
+        </View>
+        {/* <Text>{state.selectedFlight}</Text> */}
+        <View style={styles.containerDepArr}>
+          <Text style={styles.depArr}>{thisFlight.depAirport}</Text>
+          <Text　style={styles.airplane}>✈︎</Text>
+          <Text style={styles.depArr}>{thisFlight.arrAirport}</Text>
+        </View>
+      </View>
+        {/* <FlightInfo /> */}
+        <UserInfo thisFlight = {thisFlight}/>
+      
       <Picture />
     </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  flight: {
+    flex: 1,
+  },
+  flightInfo: {
+    display: "flex",
+    backgroundColor: "white",
+    margin: 15,
+    borderRadius: 8,
+    shadowColor: "#000",
+    textShadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    textShadowRadius: 3.84,
+    elevation: 5,
+  },
+  logoAirline: {
+    flexDirection: "row",
+  },
+  airline: {
+    marginBottom: 10,
+    textAlign: "center",
+    alignSelf: "flex-end",
+    color: "gray"
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignItems: "flex-end",
+  },
+  depArr: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "dimgray"
+  },
+  containerDepArr: {
+    flexDirection: "row",
+    marginBottom: 10,
+    marginLeft: 10,
+  },
+  airplane: {
+    margin: 3,
+    fontSize: 20,
+    textAlign: "center",
+    alignSelf: "center",
+    color: "dimgray"
+  }
+})
--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,16 +1,43 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList, PickerIOSComponent } from "react-native";
 
 export default function UserInfo ({thisFlight}) {
    
     return (
-        <View>
-            <Text>Purpose:{ thisFlight.purpose }</Text>
-            <Text>Entertainment:{ thisFlight.entertainment }</Text>
-            <Text>Meal:{ thisFlight.meal }</Text>
-            <Text>SeatNo:{ thisFlight.seatNo }</Text>
-            <Text>Review:{ thisFlight.review }</Text>
+        <View style = {styles.userInfo}>
+            <View style = {styles.eachUserInfo}>
+                <Text>Purpose:{ thisFlight.purpose }</Text>
+            </View>
+            <View style = {styles.eachUserInfo}>
+                <Text>Entertainment:{ thisFlight.entertainment }</Text>
+            </View>
+            <View style = {styles.eachUserInfo}>
+                <Text>Meal:{ thisFlight.meal }</Text>
+            </View>
+            <View style = {styles.eachUserInfo}>
+                <Text>SeatNo:{ thisFlight.seatNo }</Text>
+            </View>
+            <View style = {styles.eachUserInfo}>
+                <Text>Review:{ thisFlight.review }</Text>
+            </View>
         </View>
     )
-
 }
+
+const styles = StyleSheet.create ({
+    // userInfo: {
+    //     alignItems: 'center'
+    // },
+    // eachUserInfo: {
+    //     backgroundColor: "lightblue",
+    //     height: 35,
+    //     width: "90%",
+    //     margin: 3,
+    //     justifyContent: 'center',
+    //     borderRadius: 10,
+    //     paddingLeft:7,
+    //     shadowOpacity: 0.25,
+    //     elevation: 1,
+        
+    // }
+})
