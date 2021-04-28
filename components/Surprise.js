4cf153544447c2462287b754955b0c6607de762a
ysm0706glee <ysm0706glee@gmail.com>
2021-04-28 14:00:29 +0900

    feat/surprose-button-and-picture-styling

--- components/Surprise.js
+++ components/Surprise.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { View, Text, Image, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet,
+  TouchableHighlight,
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function Surprise({ id }) {
@@ -15,19 +22,42 @@ export default function Surprise({ id }) {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       {arrayOfFlightId.includes(state.selectedFlight) && clicked === false ? (
-        <Button onPress={surprise} title="surprise" />
+        <TouchableHighlight style={styles.button}>
+          <Button onPress={surprise} title="surprise" color="#939597" />
+        </TouchableHighlight>
       ) : (
         <View>
           {qrCode && (
-            <Image
-              style={{ width: 300, height: 300 }}
-              source={{ uri: qrCode.url }}
-            />
+            <Image style={styles.image} source={{ uri: qrCode.url }} />
           )}
         </View>
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  button: {
+    width: 120,
+    margin: 10,
+    padding: 5,
+    backgroundColor: "#f5df4d",
+    borderColor: "#939597",
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  image: {
+    marginTop: 20,
+    marginBottom: 20,
+    width: 350,
+    height: 300,
+    borderColor: "#298BD9",
+    borderRadius: 5,
+    borderWidth: 10,
+  },
+});
